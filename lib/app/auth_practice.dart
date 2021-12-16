51055f902f15f67930827e521d923004f1cd670d
wajahatiqbal22 <wajahatiqbal22@gmail.com>
2021-12-16 14:09:45 +0500

    .

--- lib/app/auth_practice.dart
+++ lib/app/auth_practice.dart
@@ -1,7 +1,5 @@
 import 'package:auth_practice/routes/routes.dart';
-import 'package:auth_practice/screens/home_screen.dart';
-import 'package:auth_practice/screens/login_screen.dart';
-import 'package:auth_practice/screens/register_screen.dart';
+
 import 'package:flutter/material.dart';
 
 class AuthPractice extends StatelessWidget {
